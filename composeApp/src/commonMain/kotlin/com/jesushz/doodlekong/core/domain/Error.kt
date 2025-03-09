a33d0353461b998f8462172ecc7f04b5a25f66c6
jesusclemente <clementej@globalhitss.com>
2025-03-09 00:26:34 -0600

    Setup Ktor

---
+++ composeApp/src/commonMain/kotlin/com/jesushz/doodlekong/core/domain/Error.kt
@@ -0,0 +1,3 @@
+package com.jesushz.doodlekong.core.domain
+
+interface Error
--- composeApp/src/commonMain/kotlin/com/jesushz/doodlekong/di/SharedModule.kt
+++ composeApp/src/commonMain/kotlin/com/jesushz/doodlekong/di/SharedModule.kt
@@ -1,12 +1,17 @@
 package com.jesushz.doodlekong.di
 
+import com.jesushz.doodlekong.core.data.HttpClientFactory
 import com.jesushz.doodlekong.util.DispatcherProvider
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
+import org.koin.core.module.Module
 import org.koin.dsl.module
 
-val dispatcherProviderModule = module {
+expect val platformModule: Module
+
+val sharedModule = module {
+
     single<DispatcherProvider> {
         object : DispatcherProvider {
             override val main: CoroutineDispatcher
@@ -17,4 +22,7 @@ val dispatcherProviderModule = module {
                 get() = Dispatchers.Default
         }
     }
+
+    single { HttpClientFactory.create(get()) }
+
 }
