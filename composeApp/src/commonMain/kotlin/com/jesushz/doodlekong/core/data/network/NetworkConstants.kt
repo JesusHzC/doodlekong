6c4d19f56c4297eebdeb0db7f67ee69fce8a3e23
jesusclemente <clementej@globalhitss.com>
2025-03-11 16:26:13 -0600

    Add NetworkConstants

---
+++ composeApp/src/commonMain/kotlin/com/jesushz/doodlekong/core/data/network/NetworkConstants.kt
@@ -0,0 +1,11 @@
+package com.jesushz.doodlekong.core.data.network
+
+object NetworkConstants {
+
+    const val BASE_URL = ""
+
+    const val CREATE_ROOM_ENDPOINT = "/api/createRoom"
+    const val GET_ROOMS_ENDPOINT = "/api/getRooms"
+    const val JOIN_ROOM_ENDPOINT = "/api/joinRoom"
+
+}
