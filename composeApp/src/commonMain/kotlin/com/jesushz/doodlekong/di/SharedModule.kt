e8513ade997502cce13dfbb641a9bd40e15cb8f8
jesusclemente <clementej@globalhitss.com>
2025-03-18 23:16:28 -0600

    Inject client id from data prefs

--- composeApp/src/commonMain/kotlin/com/jesushz/doodlekong/di/SharedModule.kt
+++ composeApp/src/commonMain/kotlin/com/jesushz/doodlekong/di/SharedModule.kt
@@ -1,12 +1,21 @@
+@file:OptIn(ExperimentalUuidApi::class)
+
 package com.jesushz.doodlekong.di
 
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import com.jesushz.doodlekong.core.data.network.HttpClientFactory
 import com.jesushz.doodlekong.util.DispatcherProvider
+import com.jesushz.doodlekong.util.PrefsDataStore
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 expect val platformModule: Module
 
@@ -23,6 +32,25 @@ val sharedModule = module {
         }
     }
 
-    single { HttpClientFactory.create(get()) }
+    single { HttpClientFactory.create(get(), get()) }
+
+    // Provide client Id from data store
+    single<String> {
+        val dataStore = get<PrefsDataStore>()
+        val clientIdKey = stringPreferencesKey("clientId")
+        val preferences = runBlocking { dataStore.data.first() }
+        val clientIdExists = preferences[clientIdKey] != null
+        if (clientIdExists) {
+            preferences[clientIdKey] ?: ""
+        } else {
+            val newClientId = Uuid.random().toString()
+            runBlocking {
+                dataStore.edit{ settings ->
+                    settings[clientIdKey] = newClientId
+                }
+            }
+            newClientId
+        }
+    }
 
 }
