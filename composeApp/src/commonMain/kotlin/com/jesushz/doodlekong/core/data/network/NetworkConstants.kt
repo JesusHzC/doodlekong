f62aca8721aea8a9e6a0f36e20da8d3aa76d9861
jesusclemente <clementej@globalhitss.com>
2025-03-19 14:20:15 -0600

    Update NetworkConstants

--- composeApp/src/commonMain/kotlin/com/jesushz/doodlekong/core/data/network/NetworkConstants.kt
+++ composeApp/src/commonMain/kotlin/com/jesushz/doodlekong/core/data/network/NetworkConstants.kt
@@ -5,9 +5,7 @@ object NetworkConstants {
     // Base URL of the server
     const val BASE_URL = "http://192.168.1.6:8080"
     // Socket URL of the server
-    const val SOCKET_HOST = "192.168.1.6"
-    const val SOCKET_ENDPOINT = "/ws/draw"
-    const val SOCKET_PORT = 8080
+    const val SOCKET_URL = "ws://192.168.1.6:8080/ws/draw"
 
     // Endpoints of the server
     const val CREATE_ROOM_ENDPOINT = "/api/createRoom"
@@ -23,7 +21,7 @@ object NetworkConstants {
     const val TYPE_PHASE_CHANGE = "TYPE_PHASE_CHANGE"
     const val TYPE_CHOSEN_WORD = "TYPE_CHOSEN_WORD"
     const val TYPE_GAME_STATE = "TYPE_GAME_STATE"
-    const val TYPE_NEW_WORDS = "TYPE_NEW_WORDS"
+    const val TYPE_NEW_WORDS = "TYPE_NEW_WORD"
     const val TYPE_PLAYERS_LIST = "TYPE_PLAYERS_LIST"
     const val TYPE_PING = "TYPE_PING"
     const val TYPE_DISCONNECT_REQUEST = "TYPE_DISCONNECT_REQUEST"
